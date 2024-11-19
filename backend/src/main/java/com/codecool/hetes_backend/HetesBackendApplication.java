a6f7aef45d9ff5dfdc80bd1a14f6925700c528a4
bence <bence.matuz1@gmail.com>
2024-11-19 10:10:45 +0100

    feat: initialize Spring Boot Web project

---
+++ backend/src/main/java/com/codecool/hetes_backend/HetesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.hetes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HetesBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/codecool/hetes_backend/HetesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.hetes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HetesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
